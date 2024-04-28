7e78bf9573c5e2aa05fca30b4c09d2a3545c3012
didulabhanuka <bhanukadidula78@gmail.com>
2024-04-28 09:33:59 +0530

    booking route updated

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
+const checkAuth = require('../middleware/check-auth');
+
 
 router.post('/add', checkAuth(['Student', 'Admin', 'Faculty']), bookingController.addBooking);
 router.get('/', checkAuth(['Student', 'Admin', 'Faculty']), bookingController.getBookings);
@@ -8,5 +10,5 @@ router.put('/update/:id', checkAuth(['Student', 'Admin', 'Faculty']), bookingCon
 router.delete('/delete/:id', checkAuth(['Student', 'Admin', 'Faculty']), bookingController.deleteBooking);
 router.get('/get/:id', checkAuth(['Student', 'Admin', 'Faculty']), bookingController.getBooking);
 
+
 module.exports = router;
-git 
